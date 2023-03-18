f0472e750645b756277d975a528ce79bcf226064
Benedikt Magnusson <bsm@hi.is>
2023-03-18 22:41:15 +0000

    init

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ requires = ["Sphinx>=0.6", "setuptools"]
 
 
 setup(
-    name="datacamp",
-    version="1.0.1",
+    name="sphinxcontrib-datacamp",
+    version="1.2",
     description="Sphinx datacamp extension",
     author="Arnór Pétur Marteinsson",
+    maintainer="Benedikt Magnusson",
+    maintainer_email="bsm@hi.is",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requires,
-    namespace_packages=["datacamp"],
+    namespace_packages=["sphinxcontrib"],
 )
